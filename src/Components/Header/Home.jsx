43a8aa56035ef3df6f4d358c91e20970d6e28076
Your Name <tsimbalenko.vv@gmail.com>
2026-01-07 11:09:40 +0200

    cg

--- src/Components/Header/Home.jsx
+++ src/Components/Header/Home.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import "./home.css"
 import girl from "../Image/7b4a6cb68f99340c5073c6ec1c4e9dca.jpg"
+import girl1 from"../Image/7858d44b402eff7aad9e5c34f8e77605.jpg"
 
 export default function Home() {
   return (
     <div className='home'>
        <h1>My Sweet Home</h1> 
-        <img src={girl} alt=""></img>
+        <img className="girl" src={girl} alt=""></img>
+        <img className="girl1" src={girl1} alt=""></img>
+<p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+                faucibus ex sapien vitae pellentesque sem placerat. In id cursus
+                mi pretium tellus duis convallis. Tempus leo eu aenean sed diam
+                urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
+                egestas. Iaculis massa nisl malesuada lacinia integer nunc
+                posuere. Ut hendrerit semper vel class aptent taciti sociosqu.
+                Ad litora torquent per conubia nostra inceptos himenaeos. Lorem
+                ipsum dolor sit amet consectetur adipiscing elit. Quisque
+                faucibus ex sapien vitae pellentesque sem placerat. In id cursus
+                mi pretium tellus duis convallis. Tempus leo eu aenean sed diam
+                urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
+                egestas. Iaculis massa nisl malesuada lacinia integer nunc
+                posuere. Ut hendrerit semper vel class aptent taciti sociosqu.
+                Ad litora torquent per conubia nostra inceptos himenaeos.
+              </p>
+
         </div>
   )
 }
